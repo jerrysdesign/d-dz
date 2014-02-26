f13bfff75a2c7582fc88ee444af9f370fb357cd9
jerrysdesign <jerry7722@hotmail.com>
2014-02-26 17:50:17 +0800

    helpers 與 選單 url

--- view_helpers.rb
+++ view_helpers.rb
@@ -1,4 +1,23 @@
+require "./helpers/helpers.rb"
+require "./helpers/breadcrumb.rb"
+# The url will join {http_path} variable.
+
 module ViewHelpers
+
+  extend Helpers
+  # load breadcrumb
+  $breadcrumbs = Breadcrumb::Breadcurnbs.new File.expand_path('breadcrumbs.json')
+
+  def new_link_to(name, href, options={})
+    is_reference = $breadcrumbs.found_reference request.path, href
+    href = File.join(Helpers::Path.get_http_path, href)
+    href = ensure_path(href, '')
+    if href == File.join(Helpers::Path.get_http_path, request.path) || is_reference
+      options[:class] ||=''
+      options[:class] += "active"
+    end
+    link_to name, href, options
+  end
 
   # yaml data
   def data
