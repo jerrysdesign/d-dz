b0b2499287e8222c452dbd032007d94545a4d5e9
jerrysdesign <jerry7722@hotmail.com>
2014-04-02 23:06:36 +0800

    fix innovation playground

--- javascripts/all.js
+++ javascripts/all.js
@@ -47,128 +47,13 @@ $(document).ready(function(){
 
 });
 
-//Page Innovation Stories tab
-$(document).ready(function(){
-
-  $('[class^="story-"]').hide();
-  $('[class^="story-"]:first-child').show();
-  $('.story:first-child').addClass('active');
-  //var tabMenuH = $("#tabMenu").innerHeight();
-  //var WsH = $(window).scrollTop() + $(window).innerHeight()-tabMenuH;
-  var tabMenuY = $("#tabMenu").position().top;
-
-  // .innerHeight()
-
-  $(".story").click(function() {
-    var thisId = $(this).attr('id');
-    var thatId =$(this).siblings().attr('id');
-    $('[class^="story-"]').hide();
-    $('.'+thisId).show();
-    $('.story').removeClass('active');
-    $(this).addClass('active');
-    //$(window).scrollTo(tabMenuY-tabMenuH);
-  });
-  
-  //Scroll fixed
-  var nav_pos = $('#tabMenu').position().top;
-  $(window).scroll(function() {
-    // Detect if content is being scroll offscreen.
-    if ( (document.documentElement.scrollTop || document.body.scrollTop) >= nav_pos-5) {
-      $('body').addClass('smartposition');
-    } else {
-      $('body').removeClass('smartposition');
-    }
-  });
-  $(window).scroll(); // Trigger onload
-
-  // $(window).scroll(function(){
-
-  //   if( $(window).scrollTop() > (tabMenuY)){
-  //     $("#tabMenu").addClass('active');
-  //   }
-  //   else {
-  //     $("#tabMenu").removeClass('active');
-  //   }
-  // });
-
-  // scroll tragger class
-  
-  $(document).ready(function(){
-
-    var htmlHeight = jQuery(document).height();
-
-    var p11cot = $("#p1-1").position().top;
-    var p12cot = $("#p1-2").position().top;
-    var p13cot = $("#p1-3").position().top;
-
-    var p21aot = $("#p2-1").position().top;
-    var p22aot = $("#p2-2").position().top;
-    var p23aot = $("#p2-3").position().top;
-    
-    $(window).scroll(function(){
-
-      if ($(".story-1").css("display") == 'block' ){
-
-        if ($(window).scrollTop() > (p11cot-200)){
-          $("#p1-1").addClass("move");
-        }
-        else {
-          $("#p1-1").removeClass("move");
-        }
-
-        if ($(window).scrollTop() > (p12cot-200)){
-          $("#p1-2").addClass("move");
-        }
-        else {
-          $("#p1-2").removeClass("move");
-        }
-
-        if ($(window).scrollTop() > (p13cot-200)){
-          $("#p1-3").addClass("move");
-        }
-        else {
-          $("#p1-3").removeClass("move");
-        }
-      }
-
-      if ($(".story-2").css("display") == 'block' ){
-        
-        if ($(window).scrollTop() > (p21aot-200)){
-          $("#p2-1").addClass("move");
-        }
-        else {
-          $("#p2-1").removeClass("move");
-        }
-
-        if ($(window).scrollTop() > (p22aot-200)){
-          $("#p2-2").addClass("move");
-        }
-        else {
-          $("#p2-2").removeClass("move");
-        }
-
-        if ($(window).scrollTop() > (p23aot-200)){
-          $("#p2-3").addClass("move");
-        }
-        else {
-          $("#p2-3").removeClass("move");
-        }
-      }
-
-    });
-
-  });
-
-
-});
-
 // Index slider
 var mainSlider = document.getElementById('mainSlider');
 var    bullets = document.getElementById('position').getElementsByTagName('li');
 
 window.slider1 = Swipe(mainSlider, {
   startSlide: 0,
-  speed: 600,
+  speed: 800,
   auto: 5500,
   continuous: true,
   disableScroll: true,
