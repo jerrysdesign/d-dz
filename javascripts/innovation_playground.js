8ba345d5586f1a702f1bbac2918b526a09c12928
jerrysdesign <jerry7722@hotmail.com>
2014-05-05 17:34:38 +0800

    fix

--- javascripts/innovation_playground.js
+++ javascripts/innovation_playground.js
@@ -54,7 +54,7 @@ $(function() {
       init      = function() {
         
         // (not necessary) preloading the images here...
-        $items.add('<img src="images/ajax-loader.gif"/><img src="images/black.png"/>').imagesLoaded( function() {
+        $items.add('<img src="../images/innovation/ajax-loader.gif"/><img src="../images/innovation/black.png"/>').imagesLoaded( function() {
           
           // add large image wrapper
           _addItemWrapper();
